7196842261afd37c31e6210bc81f8b5b3cac07d8
StephMarieH <stephaniemariehornby@gmail.com>
2024-05-24 08:33:52 +0100

    added input validation and ability to save input to a file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # event_ticket
+
+This is a programme where users apply for a free ticket for an event.
+The user needs to input some personal information to receive the ticket.
--- event_ticket.py
+++ event_ticket.py
@@ -1,43 +1,75 @@
-"""
-Create a programme where users apply for a free ticket for an event.
-The user needs to input some personal information to receive the ticket.
-Do input validation and exception handling for each user input submitted.
+# ---------- Functions ----------
 
-"""
+def input_validation(name_type):
+    while True:
+        user_input = input(f"Please enter your {name_type} here: ")
+        user_input = user_input.lower()
+        for char in user_input:
+            if char in '.!@£$%^&*()_+=[{]};:"\\|?>,<0123456789/':
+                print(f"ERROR: Invalid character used in input of {name_type}")
+                break
+            else:
+                return user_input
+            
+# ---------- End of Functions ----------
+
+# ---------- Introduction ----------
 
 LINE = "-" * 79
 heading = "Application for a Free Ticket to our Event!"
 
 print(f"{LINE}\n{heading}\n{LINE}")
 
-# Enter first name
-user_name = input("Enter your first name: ")
-user_name = user_name.lower()
-print(user_name)
+# ---------- End of Introduction ----------
 
-# Enter surname
-user_surname = input("Enter your surname: ")
-user_surname = user_surname.lower()
-print(user_surname)
+# ---------- Main Code ----------
+
+# Enter first name
+user_first_name = input_validation("First Name")
+
+# Enter last name
+user_last_name = input_validation("Last Name")
 
 # Enter email address
-user_email = input("Enter your email address: ")
-if "@" in user_email and "." in user_email:
-    print(user_email)
-else:
-    print("Invalid email address entered")
 
+while True:
+    user_email = input("Enter your email address: ")
+    user_email = user_email.lower()
+    if "@" in user_email and "." in user_email:
+        break
+    else:
+        print("ERROR: Invalid email address entered")
+        continue
+            
 # Enter mobile number
-user_mob_num = input("Enter a UK mobile number (starting in 07 and without spaces): ")
-if len(user_mob_num) == 11 and user_mob_num.isdigit():
-    print(user_mob_num)
-else:
-    print("Please enter a UK mobile number: ")
+while True:
+    user_mob_num = input("Enter a UK mobile number (starting in 07 and without spaces): ")
+    if user_mob_num[0] == '0' and user_mob_num[1] == '7' and len(user_mob_num) == 11:
+        break
+    else:
+        print("ERROR: Please enter a UK mobile number: ")
+        continue
 
 # Enter age (applicant must be over 18)
-user_age = input("Enter your age: ")
-if user_age.isnumeric and user_age >= 18:
-# Thank user for registering, their ticket will be emailed 24hrs before event.
-    print("Thankyou for registering, your ticket will be emailed to you 24hrs before the event starts.")
-else:
-    print("Sorry you have to be 18 or over to attend this event.")
+while True:
+    user_age = input("Enter your age: ")
+    if user_age.isdigit() and int(user_age):
+        if int(user_age) >= 18:
+            # Thank user for registering, their ticket will be emailed 24hrs before event.
+            print("Thankyou for registering.\nYour ticket will be emailed to you 24hrs before the event starts.")
+            break
+        else:
+            print("ERROR: Sorry you have to be 18 or over to attend this event.")
+            continue
+    else:
+        print("ERROR: please enter your age in a numeric format")
+
+# ---------- End of Main Code ----------
+
+# ---------- Writing Data to File ----------
+
+with open("event_applications.txt", 'a') as file_obj:
+    file_obj.write(f"{user_first_name},{user_last_name},{user_email},{user_mob_num},{user_age},\n")
+
+# ---------- End of Writing Data to File ----------
+
